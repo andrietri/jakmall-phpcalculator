13cfb5794d799edf7f0cd308ae7532fc24bea9e1
Andrie Tri Laksono <andrietrilaksono@gmail.com>
2019-07-13 17:35:11 +0700

    add command devide

--- commands.php
+++ commands.php
@@ -4,5 +4,6 @@ return [
     // TODO : Add list of commands here
     \Jakmall\Recruitment\Calculator\Commands\AddCommand::class,
     \Jakmall\Recruitment\Calculator\Commands\SubtractCommand::class,
-    \Jakmall\Recruitment\Calculator\Commands\MultiplyCommand::class
+    \Jakmall\Recruitment\Calculator\Commands\MultiplyCommand::class,
+    \Jakmall\Recruitment\Calculator\Commands\DivideCommand::class
 ];
---
+++ src/Commands/DivideCommand.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Jakmall\Recruitment\Calculator\Commands;
+
+// use Illuminate\Console\Command;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+
+class DivideCommand extends Command
+{
+    /**
+     * @var string
+     */
+    protected $description = "Divide all given Numbers";
+    /**
+     * @var string
+     */
+    protected $arguments = "The number to be divided";
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->description = $description;
+        $this->arguments = $arguments;
+    }
+
+    protected function configure()
+    {
+        $this->setName('divide')
+        ->addArgument('numbers', InputArgument::IS_ARRAY | InputArgument::REQUIRED, $this->arguments)
+        ->setDescription($this->description);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $number = $input->getArgument('numbers');
+        $result = $number[0];
+
+        if (count($number) > 1) {
+            $output->write($result.' ');
+            $output->write('/ ');
+            for ($i=1; $i < count($number); $i++) { 
+                $output->write($number[$i].' ');
+                
+                if($i == count($number) - 1){
+                    $output->write('= ');
+                } else {
+                    $output->write('/ ');
+                }
+                
+                $result /= $number[$i];
+            }
+            $output->writeln($result); 
+        } else {
+            $output->writeln($number);
+        }
+    }
+}
